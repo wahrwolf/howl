ae384fc6e39888e447c32d97894dde081f19b86f
Vincent Dahmen <wahrwolf@wolfpit.net>
2019-02-12 18:05:09 +0100

    howl: adds plugin architecture

--- howl.py
+++ howl.py
@@ -1,9 +1,11 @@
 #!/bin/env python3
 from logging import getLogger
 from logging.config import dictConfig
+from importlib import import_module
 from toml import load
 
 from howl.defaults import logger_config as DEFAULT_LOGGER_CONFIG
+from howl.Messenger import Messenger
 
 # Parse commandline args
 config_path = "./howl.toml"
@@ -16,12 +18,39 @@ try:
 except KeyError:
     LOGGER_CONFIG = DEFAULT_LOGGER_CONFIG
     # default fallback logging config
-finally :
+finally:
     dictConfig(LOGGER_CONFIG)
 
 # Init logger
 logger = getLogger()
 
-# Load all accounts
-logger.debug(CONFIG)
+logger.debug("Loading up modules...")
+modules = {}
 
+# Load up modules
+for plugin in CONFIG["modules"]:
+    name = plugin['name']
+    class_name = plugin['class']
+    logger.debug(f"Installing plugin: [{name}]")
+    try:
+        module = import_module(name)
+        logger.debug("  -Loaded module")
+        class_object = getattr(module, class_name)
+        logger.debug("  -Loaded class")
+
+        if not Messenger in class_object.__bases__:
+            raise NotImplementedError
+        else:
+            logger.debug("  -Plugin-Class is valid!")
+
+    except Exception as err:
+        logger.debug(err)
+        logger.debug(f"  -Failed to load {name}... Skiping it!")
+    else:
+        modules[name] = None
+        logger.debug(f"  -Installed {name}")
+
+# Load all accounts
+logger.debug("Loading up accounts...")
+for account in CONFIG["accounts"]:
+    logger.debug(f"  {account['name']}: Loading up module {account['module']}")
--- howl/defaults.py
+++ howl/defaults.py
@@ -3,7 +3,7 @@ from logging import DEBUG
 logger_config = {
     "version" : 1,
     "formatters" : {
-        'brief': {'format': '%(name)-8s %(message)s'}
+        'brief': {'format': '[%(levelname)s]: %(message)s'}
     }, "handlers" : {
         'console': {
             'class': 'logging.StreamHandler',
